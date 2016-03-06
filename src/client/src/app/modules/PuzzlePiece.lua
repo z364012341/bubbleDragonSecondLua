f6c36c99d5622f2639c881c061ba3b4206df3835
黄泽昊 <734689022@qq.com>
2016-03-06 13:11:17 +0800

    一块拼图碎片模块

--- src/client/src/app/modules/PuzzlePiece.lua
+++ src/client/src/app/modules/PuzzlePiece.lua
@@ -2,7 +2,7 @@
 -- Author: 黄泽昊
 -- Date: 2016-03-04 14:16:18
 -- 功能: 一块拼图碎片
---[[
+--[[params = 
 	{
         index_x = 0, --拼图碎片的x索引
         index_y = 0, --拼图碎片的y索引
@@ -14,7 +14,7 @@
     }
 ]]
 local PuzzlePiece = class("PuzzlePiece", function (params)
-	dump(params)
+	--dump(params)
     return cc.ClippingNode:create(require(PUZZLE_STENCIL_PATH):create(params.left, 
     	params.right, params.top, params.bottom));
 end)
@@ -23,12 +23,43 @@ local PuzzleBottomPlate = require(PUZZLE_BOTTOM_PLATE_PATH);
 function PuzzlePiece:ctor(params)
     printf("PuzzlePiece");
     self:addChild(PuzzleBottomPlate:create(params.index_x, params.index_y, params.path));
-    self:setAlphaThreshold(0.00);
+    self:setAlphaThreshold(0.1);
+
+    self:addTouchEvent();
 end
 
 function PuzzlePiece:getPosWithIndex(index_x, index_y)
 	local posX = PUZZLE_STENCIL_WIDTH * (0.5 + index_x);
 	local posY = PUZZLE_STENCIL_HEIGHT * (0.5 + index_y);
 	return -1*posX, -1*posY;
+end
+
+function PuzzlePiece:addTouchEvent()
+    local listener = cc.EventListenerTouchOneByOne:create();
+    listener:setSwallowTouches(true);
+    listener:registerScriptHandler(self.onTouchBegan,cc.Handler.EVENT_TOUCH_BEGAN);
+    listener:registerScriptHandler(self.onTouchMoved,cc.Handler.EVENT_TOUCH_MOVED);
+    listener:registerScriptHandler(self.onTouchEnded,cc.Handler.EVENT_TOUCH_ENDED);
+    cc.Director:getInstance():getEventDispatcher():addEventListenerWithSceneGraphPriority(listener, self);
+end
+
+function PuzzlePiece.onTouchBegan(touch, event)
+    --printf("PuzzlePiece onTouchBegan");
+    return PuzzlePiece.isTouchOnPuzzle(touch, event);
+end
+
+function PuzzlePiece.onTouchMoved(touch, event)
+    --printf("PuzzlePiece onTouchMoved");
+    local node = event:getCurrentTarget();
+    node:setPosition(node:getParent():convertTouchToNodeSpace(touch));
+end
+
+function PuzzlePiece.onTouchEnded(touch, event)
+    printf("PuzzlePiece onTouchEnded");
+end
+
+function PuzzlePiece.isTouchOnPuzzle(touch, event)
+    local node = event:getCurrentTarget();
+    return cc.pGetDistance(node:convertTouchToNodeSpace(touch), cc.p(0, 0)) < PUZZLE_STENCIL_WIDTH/2;
 end
 return PuzzlePiece
