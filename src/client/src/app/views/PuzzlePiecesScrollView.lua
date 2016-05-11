60869827e5a57152509956a61ed7fdddac1d501c
黄泽昊 <734689022@qq.com>
2016-05-11 21:26:01 +0800

    Signed-off-by: 黄泽昊 <734689022@qq.com>

--- src/client/src/app/views/PuzzlePiecesScrollView.lua
+++ src/client/src/app/views/PuzzlePiecesScrollView.lua
@@ -18,22 +18,22 @@ end
 
 function PuzzlePiecesScrollView:init(puzzleTable)
 	local pos_x = PUZZLE_PIECES_INITIAL_POS_X;
-	local scrollView = ccui.ScrollView:create();
-	scrollView:setPosition(0, 0);
-	scrollView:setContentSize(cc.size(cc.Director:getInstance():getVisibleSize().width, PUZZLE_PIECES_DESK_HEIGHT));
-	scrollView:setBounceEnabled(true);
-	scrollView:setScrollBarEnabled(false);
-	scrollView:setDirection(cc.SCROLLVIEW_DIRECTION_HORIZONTAL);
+	self.scrollView_ = ccui.ScrollView:create();
+	self.scrollView_:setPosition(0, 0);
+	self.scrollView_:setContentSize(cc.size(cc.Director:getInstance():getVisibleSize().width, PUZZLE_PIECES_DESK_HEIGHT));
+	self.scrollView_:setBounceEnabled(true);
+	self.scrollView_:setScrollBarEnabled(false);
+	self.scrollView_:setDirection(cc.SCROLLVIEW_DIRECTION_HORIZONTAL);
 	for _, puzzle in ipairs(puzzleTable) do
 		puzzle:setPosition(cc.p(pos_x, PUZZLE_PIECES_POS_Y));
 		puzzle:setScale(PUZZLE_PIECES_SCALE);
 		pos_x = pos_x + PUZZLE_STENCIL_WIDTH;
-		scrollView:addChild(puzzle);
+		self.scrollView_:addChild(puzzle);
 		puzzle:setPuzzlePieceMoveNode(self);
 	end
-	scrollView:setInnerContainerSize(cc.size(pos_x, PUZZLE_PIECES_DESK_HEIGHT));
-	self:addChild(scrollView);
-	self:setContentSize(scrollView:getContentSize());
+	self.scrollView_:setInnerContainerSize(cc.size(pos_x, PUZZLE_PIECES_DESK_HEIGHT));
+	self:addChild(self.scrollView_);
+	self:setContentSize(self.scrollView_:getContentSize());
 end
 
 function PuzzlePiecesScrollView:moveOutPuzzlePiece(puzzle)
@@ -63,6 +63,23 @@ function PuzzlePiecesScrollView:adjustPuzzlePiecesPosition()
 end
 
 function PuzzlePiecesScrollView:insertPuzzlePiece(puzzle)
-	printf("11111111");
+	if #self.puzzle_table_ == 0 then
+		table.insert(self.puzzle_table_, puzzle);
+		return;
+	end
+	local point = self.scrollView_:getInnerContainer():convertToNodeSpace(puzzle:getParent():convertToWorldSpace(cc.p(puzzle:getPosition())));
+	for i,v in ipairs(self.puzzle_table_) do
+		if point.x < v:getPositionX() then
+			table.insert(self.puzzle_table_, i,  puzzle);
+			puzzle:retain();
+			puzzle:removeFromParent();
+			puzzle:setPosition(point);
+			self.scrollView_:addChild(puzzle);
+			puzzle:release();
+			break;
+		end
+	end
+
+	self:adjustPuzzlePiecesPosition();
 end
 return PuzzlePiecesScrollView
