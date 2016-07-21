6e4a923658319c8950cc82b968490063ba85d003
黄泽昊 <734689022@qq.com>
2016-07-21 22:26:07 +0800

    Signed-off-by: 黄泽昊 <734689022@qq.com>

--- src/client/frameworks/runtime-src/Classes/GameAudioController.cpp
+++ src/client/frameworks/runtime-src/Classes/GameAudioController.cpp
@@ -29,8 +29,66 @@ const std::map<std::string, std::string> GAME_EFFECT_PATH = {
 };
 namespace bubble_second {
     using CocosDenshion::SimpleAudioEngine;
+    std::string transform(const std::string& str)
+    {
+        std::string tempStr = "";
+        for (auto var : str)
+        {
+            if (var < 'a')
+            {
+                tempStr += var + 'a' - 'A';
+            }
+            else
+            {
+                tempStr += var;
+            }
+        }
+        tempStr[0] -= 'a' - 'A';
+        return  tempStr;
+    }
+    std::string specialSplit(const std::string& str, const char& separator)
+    {
+        std::string result = "";
+        std::string tempStr = "";
+        for (auto var : str)
+        {
+            if (var != separator)
+            {
+                tempStr += var;
+            }
+            else
+            {
+                result += transform(tempStr);
+                tempStr = "";
+            }
+        }
+        result += transform(tempStr);
+        return result;
+    }
     GameAudioController::GameAudioController()
     {
+        //{//打印音效的声明和实现
+        //    using std::string;
+        //    string declaration = "";
+        //    for (auto var : GAME_EFFECT_PATH)
+        //    {
+        //        declaration += "void play" + specialSplit(var.first, '_') + "();\n";
+        //    }
+        //    CCLOG("%s", declaration.c_str());
+
+        //    string implement = "";
+        //    for (auto var : GAME_EFFECT_PATH)
+        //    {
+        //        implement += "void GameAudioController::play" + specialSplit(var.first, '_') +
+        //            "()\n{\n    this->playEffectWithKey(\"" + var.first + "\");\n}\n";
+        //    }
+        //    CCLOG("%s", implement.c_str());
+        //}
+    }
+
+    void GameAudioController::playEffectWithKey(const std::string & key)
+    {
+        SimpleAudioEngine::getInstance()->playEffect(GAME_EFFECT_PATH.at(key).c_str());
     }
 
 
@@ -45,5 +103,93 @@ namespace bubble_second {
             SimpleAudioEngine::getInstance()->preloadEffect(var.second.c_str());
         }
 
+    }
+    void GameAudioController::playBombBombBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("BOMB_BOMB_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playColorBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("COLOR_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playContactBarrelBottomEffect()
+    {
+        this->playEffectWithKey("CONTACT_BARREL_BOTTOM_EFFECT");
+    }
+    void GameAudioController::playContactBarrelHeadEffect()
+    {
+        this->playEffectWithKey("CONTACT_BARREL_HEAD_EFFECT");
+    }
+    void GameAudioController::playContactMutiplesealButtleEffect()
+    {
+        this->playEffectWithKey("CONTACT_MUTIPLESEAL_BUTTLE_EFFECT");
+    }
+    void GameAudioController::playContactRainbowsealBubbleEffect()
+    {
+        this->playEffectWithKey("CONTACT_RAINBOWSEAL_BUBBLE_EFFECT");
+    }
+    void GameAudioController::playContactScoreWidgetEffect()
+    {
+        this->playEffectWithKey("CONTACT_SCORE_WIDGET_EFFECT");
+    }
+    void GameAudioController::playDarkCloudBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("DARK_CLOUD_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playGameCompletedTaskFlyEndEffect()
+    {
+        this->playEffectWithKey("GAME_COMPLETED_TASK_FLY_END_EFFECT");
+    }
+    void GameAudioController::playGameStartEffect()
+    {
+        this->playEffectWithKey("GAME_START_EFFECT");
+    }
+    void GameAudioController::playGameVictoryStartTurnOnEffect()
+    {
+        this->playEffectWithKey("GAME_VICTORY_START_TURN_ON_EFFECT");
+    }
+    void GameAudioController::playGameWoodenhammerAttackEffect()
+    {
+        this->playEffectWithKey("GAME_WOODENHAMMER_ATTACK_EFFECT");
+    }
+    void GameAudioController::playLightningBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("LIGHTNING_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playPropBigBombBombBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("PROP_BIG_BOMB_BOMB_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playPropColorBombBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("PROP_COLOR_BOMB_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playRainbowsealBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("RAINBOWSEAL_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playSkillBigBombBombBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("SKILL_BIG_BOMB_BOMB_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playSkillColorBombBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("SKILL_COLOR_BOMB_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playSkillDyingBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("SKILL_DYING_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playSkillStavesBubbleEliminatedEffect()
+    {
+        this->playEffectWithKey("SKILL_STAVES_BUBBLE_ELIMINATED_EFFECT");
+    }
+    void GameAudioController::playStageButtonEffect()
+    {
+        this->playEffectWithKey("STAGE_BUTTON_EFFECT");
+    }
+    void GameAudioController::playStageUnlockEffect()
+    {
+        this->playEffectWithKey("STAGE_UNLOCK_EFFECT");
     }
 }
--- src/client/frameworks/runtime-src/Classes/GameAudioController.h
+++ src/client/frameworks/runtime-src/Classes/GameAudioController.h
@@ -17,9 +17,32 @@ namespace bubble_second {
         GETINSTANCE_FUNC(GameAudioController);
         ~GameAudioController();
         void loadAudioFile();
+
+        void playBombBombBubbleEliminatedEffect();
+        void playColorBubbleEliminatedEffect();
+        void playContactBarrelBottomEffect();
+        void playContactBarrelHeadEffect();
+        void playContactMutiplesealButtleEffect();
+        void playContactRainbowsealBubbleEffect();
+        void playContactScoreWidgetEffect();
+        void playDarkCloudBubbleEliminatedEffect();
+        void playGameCompletedTaskFlyEndEffect();
+        void playGameStartEffect();
+        void playGameVictoryStartTurnOnEffect();
+        void playGameWoodenhammerAttackEffect();
+        void playLightningBubbleEliminatedEffect();
+        void playPropBigBombBombBubbleEliminatedEffect();
+        void playPropColorBombBubbleEliminatedEffect();
+        void playRainbowsealBubbleEliminatedEffect();
+        void playSkillBigBombBombBubbleEliminatedEffect();
+        void playSkillColorBombBubbleEliminatedEffect();
+        void playSkillDyingBubbleEliminatedEffect();
+        void playSkillStavesBubbleEliminatedEffect();
+        void playStageButtonEffect();
+        void playStageUnlockEffect();
     private:
         GameAudioController();
-
+        void playEffectWithKey(const std::string& key);
     };
 }
 #endif
