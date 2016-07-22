0ee2452a754461d02f5ffb1f43a94c5a3325167c
黄泽昊 <734689022@qq.com>
2016-07-22 17:38:18 +0800

    xxx

--- src/client/frameworks/runtime-src/Classes/ButtonEffectController.cpp
+++ src/client/frameworks/runtime-src/Classes/ButtonEffectController.cpp
@@ -1,11 +1,17 @@
 #include "ButtonEffectController.h"
-//#include "BubbleSecondConstant.h"
+#include "GameAudioController.h"
 const float GAME_BUTTON_ZOOM_SCALE = -0.1f;    //BUTTON点击时的放大比例
 namespace bubble_second {
 
     void ButtonEffectController::setButtonZoomScale(cocos2d::ui::Button * button)
     {
         button->setZoomScale(GAME_BUTTON_ZOOM_SCALE);
+        button->addTouchEventListener([=](cocos2d::Ref*, cocos2d::ui::Widget::TouchEventType type) {
+            if (type == cocos2d::ui::Widget::TouchEventType::ENDED)
+            {
+                GameAudioController::getInstance()->playStageButtonEffect();
+            }
+        });
     }
 
     void ButtonEffectController::setButtonsZoomScale(cocos2d::Node * rootNode)
